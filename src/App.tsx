9951128c6a05658791d491861dabc9e28be8cf2c
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-04-09 16:00:22 -0600

    feat(router): :sparkles: add new routes
    add new routes: hotels, hotels/id/images, hotels/id/amenitys

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css'
 import Courses from './components/courses/Courses'
 import Nav from './components/nav/Nav'
 import Login from './components/login/Login'
+import { HomePrivate } from './components/Layouts/HomePrivate'
+import { HotelsLayout } from './components/hotels/HotelsLayout'
+import { HotelsAmenitesLayout } from './components/hotels/amenities/HotelsAmenitesLayout'
+import { HotelsImagesLayout } from './components/hotels/images/HotelsImagesLayout'
 
 function App() {
   
@@ -14,6 +18,15 @@ function App() {
       <Routes> 
         <Route path='/' element={<Courses />}/>
         <Route path='/login' element={<Login />} />
+
+        <Route path="private" element={<HomePrivate />}>
+              <Route index element={<HotelsLayout />} /> {/* Default route for private section */}
+              <Route path="hotels" element={<HotelsLayout />} /> 
+              <Route path="hotels/:hotelId" element={<h1>Hotel Details</h1>} />
+              <Route path="hotels/:hotelId/images" element={<HotelsImagesLayout />} />
+              <Route path="hotels/:hotelId/amenities" element={<HotelsAmenitesLayout />} />
+              <Route path="*" element={<h1>404 Not Found</h1>} />
+            </Route>
       </Routes>
       
       
