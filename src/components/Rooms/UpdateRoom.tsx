fa538733382491d296c3f523de91ff15f2c856ee
d3val <devalt5008@gmail.com>
2025-04-10 22:29:44 -0600

    search room before update room

--- src/components/Rooms/UpdateRoom.tsx
+++ src/components/Rooms/UpdateRoom.tsx
@@ -14,26 +14,66 @@ let changeRoomIDValue = (e: ChangeEvent<HTMLInputElement>) => {
 const sessionToken = useContext(authContext)?.token
 const navigate = useNavigate()
 
-function searchRoom() : void
-    {
+const [roomWasFound, setRoomWasFound] = useState(false)
 
-        axios.get<IRoom>(`http://localhost:8080/rooms/id/${roomID}`,
-            {
-              headers:{
-                Authorization:`Bearer ${sessionToken}`
-              }
-            }
-          )
-          .then((res) => {
-            console.log(res.data)
-          })
-          .catch((err) =>{
-            console.log(err)
-            console.log("Caracoles")
-          })
+const [foundRoom, setFoundRoom] = useState<IRoom|null>(null)
+
+async function searchRoom(): Promise<void> {
+    try {
+      const response = await axios.get<IRoom>(
+        `http://localhost:8080/rooms/id/${roomID}`,
+        {
+          headers: {
+            Authorization: `Bearer ${sessionToken}`,
+          },
+        }
+      );
+  
+      const room = response.data;
+      setFoundRoom(room);
+      console.log(room);
+  
+      setPrice(String(room.price));
+      setRoomNumber(String(room.roomNumber));
+      setRoomCapacity(String(room.capacity));
+      setRoomType(String(room.roomType));
+      setRoomWasFound(true);
+  
+    } catch (error) {
+      console.log(error);
+      console.log("Caracoles");
+      setRoomWasFound(false);
+    }
+  }
+  
+
+
+    const [roomNumber, setRoomNumber] = useState('')
+
+    let changeRoomNumber = (e: ChangeEvent<HTMLInputElement>) =>{
+        setRoomNumber(e.target.value)
+    }
+
+    const [roomCapacity, setRoomCapacity] = useState('')
+
+    let changeCapacity = (e: ChangeEvent<HTMLInputElement>) =>{
+        setRoomCapacity(e.target.value)
+    }
+
+    const [price, setPrice] = useState('')
+
+    let changePrice = (e: ChangeEvent<HTMLInputElement>) =>{
+        setPrice(e.target.value)
+    }
+
+    const [roomType, setRoomType] = useState('')
+
+    let changeRoomType = (e: ChangeEvent<HTMLInputElement>) =>{
+        setRoomType(e.target.value)
     }
 
   return (
+    <>
     <div>
            <button onClick={()=>navigate("/rooms")}>Back</button>
         <br/>
@@ -45,6 +85,36 @@ function searchRoom() : void
       <br/>
       <button onClick={searchRoom}>Search</button>
     </div>
+
+{ roomWasFound &&
+    <div>
+      <br/>
+      <br/>
+      <label>
+        Room number <input type="text" value={roomNumber} onChange={changeRoomNumber}/>
+      </label>
+      <br/>
+      <br/>
+      <label>
+        Capacity <input type="text" value={roomCapacity} onChange={changeCapacity}/>
+      </label>
+      <br/>
+      <br/>
+      <label>
+        Price <input type="text" value={price} onChange={changePrice}/>
+      </label>
+      <br/>
+      <br/>
+      <label>
+        Room type <input type="text" value={roomType} onChange={changeRoomType}/>
+      </label>
+      <br/>
+        <br/>
+        <br/>
+        <button onClick={UpdateRoom}>Update</button>
+    </div>
+}
+    </>
   )
 }
 
